4b59c907caad2d38273112219375ab7088b434f3
Kevin Parine <kparine314@gmail.com>
2020-09-22 23:28:05 -0700

    Update polling interval to std

--- src/__DataCard.test.js
+++ src/__DataCard.test.js
@@ -2,19 +2,22 @@ import React from "react";
 import { StateContext, StateContextProvider } from "./context/stateContext";
 import { render, wait } from "@testing-library/react";
 import DataCard from "./components/Data-Card/data-card";
+import NotificationList from "./components/Notification/notification-list";
 
 const responses = [
 	{ data: { altitude: 123, last_updated: "2020-09-23T02:19:37.860Z" } },
 	{ data: { altitude: 126, last_updated: "2020-09-23T02:19:47.860Z" } },
 	{ data: { altitude: 173, last_updated: "2020-09-23T02:19:57.860Z" } },
-	{ data: { altitude: 173, last_updated: "2020-09-23T02:19:57.860Z" } },
+	{ data: { altitude: 173, last_updated: "2020-09-23T02:20:07.860Z" } },
+	{ data: { altitude: 123, last_updated: "2020-09-23T02:20:17.860Z" } },
+	{ data: { altitude: 123, last_updated: "2020-09-23T02:20:27.860Z" } },
 ];
 
 function mockGetData() {
 	return responses.shift();
 }
 
-const TestComponent = () => {
+const TestDataCardComponent = () => {
 	return (
 		<StateContextProvider value={StateContext}>
 			<DataCard getData={mockGetData} />
@@ -23,11 +26,33 @@ const TestComponent = () => {
 };
 
 describe("<DataCard />", () => {
-	it("accept and process data from api", async () => {
-		const { getByText } = render(<TestComponent />);
+	it("accepts and processes Current Altitude data from api", async () => {
+		const { getByText } = render(<TestDataCardComponent />);
 		await wait();
 		expect(getByText("Current Altitude: 123 km")).toBeInTheDocument();
 		await wait();
 		expect(getByText("Current Altitude: 126 km")).toBeInTheDocument();
+	});
+	it("accepts and calculates Average Altitude data from api", async () => {
+		const { getByText } = render(<TestDataCardComponent />);
+		await wait();
+		expect(getByText("Current Altitude: 173 km")).toBeInTheDocument();
+		await wait();
+		expect(getByText("Average Altitude: 173 km")).toBeInTheDocument();
+	});
+});
+
+const TestNotifcationsComponent = () => {
+	return (
+		<StateContextProvider value={StateContext}>
+			<NotificationList />
+		</StateContextProvider>
+	);
+};
+
+describe("<NotificationList />", () => {
+	it("renders on initial mount", async () => {
+		const { getByText } = render(<TestNotifcationsComponent />);
+		expect(getByText("Status Log")).toBeInTheDocument();
 	});
 });
--- src/constants/param-constants.js
+++ src/constants/param-constants.js
@@ -1,3 +1,3 @@
 export const POLLING_INTERVAL_PARAM = 10000;
-export const WARNING_TIME_PARAM = 20000;
+export const WARNING_TIME_PARAM = 60000;
 export const LOW_AVG_PARAM = 160;
